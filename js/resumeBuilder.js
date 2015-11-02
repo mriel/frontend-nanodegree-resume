6b5b380c6d9dd834af427e9f4172755faa188cba
therealmatt84 <therieldesign@gmail.com>
2015-11-02 15:34:25 -0600

    update to lesson 1 data types

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
- $("#main").append("Matt");
+ //$("#main").append("Matt");
+
+ var formattedName = HTMLheaderName.replace("%data%", "Matthew Riel");
+ $("#header").prepend(formattedName);
+
+ var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Developer");
+ $("#header").append(formattedRole);
